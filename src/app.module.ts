42dcd37023dfe93abaaa0f6e7af81832d83b32b4
NicholasTavares <nicholasbalby@hotmail.com>
2023-04-25 22:44:30 -0300

    feat: config typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './modules/users/users.service';
 import { UsersModule } from './modules/users/users.module';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.get('database'),
+      }),
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [UsersService],
 })
