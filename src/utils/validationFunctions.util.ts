0f6dc006a642bce2127a95d545cb964f99ef9465
NicholasTavares <nicholasbalby@hotmail.com>
2023-04-25 22:53:04 -0300

    feat: is password valid util function

--- src/utils/validationFunctions.util.ts
+++ src/utils/validationFunctions.util.ts
@@ -2,3 +2,9 @@ export const isNameValid = (name: string): boolean => {
   const MIN_CHARACTERS = 6;
   return name.length < MIN_CHARACTERS;
 };
+
+export const isPasswordValid = (password: string): boolean => {
+  const passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{6,}$/;
+  return passwordRegex.test(password);
+};
