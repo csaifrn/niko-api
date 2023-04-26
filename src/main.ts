935a170b1170135d21aeaa4d481dfd224b16db8a
NicholasTavares <nicholasbalby@hotmail.com>
2023-04-25 23:27:54 -0300

    feat: validation pipeline for dto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { formatErrors } from './utils/formatErrors.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) =>
+        new BadRequestException(formatErrors(errors)),
+    }),
+  );
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
