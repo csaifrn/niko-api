63196b3279ac31c071d1f8516097cbd1a305d5dc
NicholasTavares <nicholasbalby@hotmail.com>
2023-04-25 23:42:10 -0300

    feat: create user service

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,71 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { CreatedUserResponse } from './interfaces/create-user-response.interface';
+import * as validation from '../../utils/validationFunctions.util';
+import { hash } from 'bcrypt';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  public async create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  }: CreateUserDTO): Promise<CreatedUserResponse> {
+    if (validation.isNameValid(name)) {
+      throw new BadRequestException('Nome deve ter ao menos 6 caracteres.');
+    }
+
+    if (!validation.isEmailValid(email)) {
+      throw new BadRequestException('Email invalido.');
+    }
+
+    if (!validation.isPasswordValid(password)) {
+      throw new BadRequestException(
+        'A senha deve conter no minimo 6 caracters, letras maiusculas e minusculas, numeros, e caracters especiais.',
+      );
+    }
+
+    if (password !== passwordConfirm) {
+      throw new BadRequestException(
+        'Senha e confimacao de senha sao diferentes.',
+      );
+    }
+
+    const existingUserCount = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .take(1)
+      .getCount();
+
+    if (existingUserCount) {
+      throw new BadRequestException(
+        'Email ja existe. Por favor, use outro email.',
+      );
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = this.userRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await this.userRepository.save(user);
+
+    return {
+      id: savedUser.id,
+      name: savedUser.name,
+      email: savedUser.email,
+    };
+  }
+}
