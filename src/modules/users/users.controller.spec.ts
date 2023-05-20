ea6ba5b61d61fc87671ce308a2d0f1294e9c02a9
NicholasTavares <nicholasbalby@hotmail.com>
2023-05-20 11:38:09 -0300

    test: create user controller

---
+++ src/modules/users/users.controller.spec.ts
@@ -0,0 +1,64 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+
+describe('UsersController', () => {
+  let controller: UsersController;
+  let service: UsersService;
+
+  const uuidPattern =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: {
+            create: jest.fn().mockResolvedValue({
+              id: '5b1ee27d-1e3f-4aad-be5e-3be6fd7fea78',
+              name: 'Nicholas Balby',
+              email: 'nicholas@email.com',
+            }),
+            findOne: jest.fn().mockResolvedValue({
+              id: '5b1ee27d-1e3f-4aad-be5e-3be6fd7fea78',
+              name: 'Nicholas Balby',
+              email: 'nicholas@email.com',
+            }),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<UsersController>(UsersController);
+    service = module.get<UsersService>(UsersService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should create an user', async () => {
+      const body: CreateUserDTO = {
+        name: 'Nicholas Balby',
+        email: 'nicholas@email.com',
+        password: 'U$er123',
+        passwordConfirm: 'U$er123',
+      };
+
+      const newUser = await controller.create(body);
+
+      expect(newUser).toMatchObject({
+        name: 'Nicholas Balby',
+        email: 'nicholas@email.com',
+      });
+      expect(newUser.id).toMatch(uuidPattern);
+      expect(service.create).toHaveBeenCalledTimes(1);
+      expect(service.create).toHaveBeenCalledWith(body);
+    });
+  });
+});
