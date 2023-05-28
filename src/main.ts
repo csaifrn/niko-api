31db04f1d7a3c647f05f60e2e2ae750b8f9ce46e
NicholasTavares <nicholasbalby@hotmail.com>
2023-05-28 10:03:50 -0300

    refactor: enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { formatErrors } from './utils/formatErrors.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
