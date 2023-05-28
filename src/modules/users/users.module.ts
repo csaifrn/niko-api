bf48ac9f489dc8a605b2e5b950ccdda029bd0f94
NicholasTavares <nicholasbalby@hotmail.com>
2023-05-28 11:36:08 -0300

    feat: import JobsModule and ResetPasswordTokenModule

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { JobsModule } from '../jobs/jobs.module';
+import { ResetPasswordTokenModule } from '../reset_password_token/reset_password_token.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    forwardRef(() => JobsModule),
+    ResetPasswordTokenModule,
+  ],
   providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService],
