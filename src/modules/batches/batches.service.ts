a0bbb5341443cc031cec6b43c3e834fa9ce632cc
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-16 19:29:05 -0300

    feat: update batch service

--- src/modules/batches/batches.service.ts
+++ src/modules/batches/batches.service.ts
@@ -1,10 +1,16 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { Batch } from './entities/batche.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateBatchDTO } from './dto/create-batch.dto';
 import { CreatedBatchResponse } from './interfaces/create-batch-response.interface';
 import * as validation from '../../utils/validationFunctions.util';
+import { UpdateBatchDTO } from './dto/update-batch.dto';
+import { UpdatedBatchResponse } from './interfaces/updated-batch-response.interface';
 
 @Injectable()
 export class BatchesService {
@@ -29,6 +35,41 @@ export class BatchesService {
       ...createBatchDTO,
       user_id,
     });
+
+    const savedBatch = await this.batchRepository.save(batch);
+
+    return {
+      id: savedBatch.id,
+      settlement_project: savedBatch.settlement_project,
+    };
+  }
+
+  public async update(
+    batch_id: string,
+    updateBatchDTO: UpdateBatchDTO,
+  ): Promise<UpdatedBatchResponse> {
+    const batch = await this.batchRepository.findOne({
+      where: {
+        id: batch_id,
+      },
+      select: ['id', 'settlement_project'],
+    });
+
+    if (!batch) {
+      throw new NotFoundException('Projeto de assentamento não encontrado.');
+    }
+
+    if (!updateBatchDTO.settlement_project) {
+      return batch;
+    }
+
+    const filteredDTO = Object.fromEntries(
+      Object.entries(updateBatchDTO).filter(
+        ([, value]) => value !== null && value !== undefined,
+      ),
+    );
+
+    Object.assign(batch, filteredDTO);
 
     const savedBatch = await this.batchRepository.save(batch);
 
