8948d4ef18cbc1064ac020e9438252cb673ff9a0
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-16 15:30:13 -0300

    feat: batch entity

---
+++ src/modules/batches/entities/batche.entity.ts
@@ -0,0 +1,35 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('batches')
+export class Batch {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  settlement_project: string;
+
+  @ManyToOne(() => User, (user) => user, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}
