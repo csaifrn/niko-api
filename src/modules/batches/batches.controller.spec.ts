834abc36f10717f73ea65301a275dfe668f5937f
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-16 19:29:18 -0300

    test: update batch controller

--- src/modules/batches/batches.controller.spec.ts
+++ src/modules/batches/batches.controller.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { BatchesController } from './batches.controller';
 import { CreateBatchDTO } from './dto/create-batch.dto';
 import { BatchesService } from './batches.service';
+import { UpdateBatchDTO } from './dto/update-batch.dto';
 
 describe('BatchesController', () => {
   let controller: BatchesController;
@@ -11,6 +12,7 @@ describe('BatchesController', () => {
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
 
   const user_id = '5b1ee27d-1e3f-4aad-be5e-3be6fd7fea78';
+  const batch_id = 'bca41e37-ef76-4489-8d5e-df0304d5517a';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -22,6 +24,10 @@ describe('BatchesController', () => {
             create: jest.fn().mockResolvedValue({
               id: '5b1ee27d-1e3f-4aad-be5e-3be6fd7fea78',
               settlement_project: 'Projeto Assentamento',
+            }),
+            update: jest.fn().mockResolvedValue({
+              id: batch_id,
+              settlement_project: 'Projeto Assentamento Santa Cruz',
             }),
           },
         },
@@ -52,6 +58,23 @@ describe('BatchesController', () => {
       expect(newBatch.id).toMatch(uuidPattern);
       expect(service.create).toHaveBeenCalledTimes(1);
       expect(service.create).toHaveBeenCalledWith(body, user_id);
+    });
+  });
+
+  describe('update', () => {
+    it('should update a batch', async () => {
+      const body: UpdateBatchDTO = {
+        settlement_project: 'Projeto Assentamento Santa Cruz',
+      };
+
+      const updatedBatch = await controller.update(batch_id, body);
+
+      expect(updatedBatch).toMatchObject({
+        settlement_project: 'Projeto Assentamento Santa Cruz',
+      });
+      expect(updatedBatch.id).toMatch(uuidPattern);
+      expect(service.update).toHaveBeenCalledTimes(1);
+      expect(service.update).toHaveBeenCalledWith(batch_id, body);
     });
   });
 });
