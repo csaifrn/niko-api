f8ad002eac0acbc21ef13f26f69ba83e492c33fa
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-16 14:09:56 -0300

    feat: add user role enum

---
+++ src/modules/users/enums/user-role.enum.ts
@@ -0,0 +1,8 @@
+// TODO: consultar as funções de cada um e ver o que o cliente há;
+// TODO: checar se o cliente possui mais privilégios que o MANAGER
+export enum UserRole {
+  OPERATOR = 1,
+  COORDINATOR = 2,
+  MANAGER = 3,
+  CLIENT = 4,
+}
