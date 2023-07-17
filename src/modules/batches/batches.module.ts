174a653d1dcd878e0b5632e8c4b1c6c845147307
NicholasTavares <nicholasbalby@hotmail.com>
2023-07-17 09:32:55 -0300

    refactor: add batch module

--- src/modules/batches/batches.module.ts
+++ src/modules/batches/batches.module.ts
@@ -3,9 +3,10 @@ import { BatchesController } from './batches.controller';
 import { BatchesService } from './batches.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Batch } from './entities/batche.entity';
+import { BatchObservation } from './entities/batche_observations.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Batch])],
+  imports: [TypeOrmModule.forFeature([Batch, BatchObservation])],
   controllers: [BatchesController],
   providers: [BatchesService],
   exports: [BatchesService],
