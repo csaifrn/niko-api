f41ad111300d172b582f3fd65aba77fe891ca565
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-14 10:34:59 -0300

    feat: setup documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { formatErrors } from './utils/formatErrors.util';
+import { setupDocumentation } from './common/documentation/build-swagger.document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -16,6 +17,11 @@ async function bootstrap() {
         new BadRequestException(formatErrors(errors)),
     }),
   );
+
+  if (process.env.API_MODE === 'DEV') {
+    setupDocumentation(app);
+  }
+
   await app.listen(process.env.APP_PORT);
 }
 bootstrap();
