816ee26fcb9d5776161571b5885958795782ccb4
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-14 10:28:58 -0300

    refactor: include new properties to response update batch

--- src/modules/batches/interfaces/updated-batch-response.interface.ts
+++ src/modules/batches/interfaces/updated-batch-response.interface.ts
@@ -1,4 +1,8 @@
 export interface UpdatedBatchResponse {
   id: string;
   settlement_project: string;
+  physical_files_count: number;
+  digital_files_count: number;
+  priority: boolean;
+  updated_at: Date;
 }
