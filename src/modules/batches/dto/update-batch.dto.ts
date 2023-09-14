28a6aa3b461496014d6e9ff9de09210092025fde
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-14 10:29:16 -0300

    refactor: include new properties to update batch dto

--- src/modules/batches/dto/update-batch.dto.ts
+++ src/modules/batches/dto/update-batch.dto.ts
@@ -1,5 +1,5 @@
 import { OmitType, PartialType } from '@nestjs/mapped-types';
-import { IsOptional, IsString } from 'class-validator';
+import { IsBoolean, IsNumber, IsOptional, IsString } from 'class-validator';
 import { CreateBatchDTO } from './create-batch.dto';
 
 export class UpdateBatchDTO extends PartialType(
@@ -8,4 +8,16 @@ export class UpdateBatchDTO extends PartialType(
   @IsString()
   @IsOptional()
   readonly settlement_project?: string;
+
+  @IsNumber()
+  @IsOptional()
+  readonly digital_files_count?: number;
+
+  @IsNumber()
+  @IsOptional()
+  readonly physical_files_count?: number;
+
+  @IsBoolean()
+  @IsOptional()
+  readonly priority?: boolean;
 }
