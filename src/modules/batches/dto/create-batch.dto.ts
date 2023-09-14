2eb8f57fa635b0dbca38e1c433378317cc2f457b
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-13 23:05:38 -0300

    refactor: add physical_files_count and priority fields

--- src/modules/batches/dto/create-batch.dto.ts
+++ src/modules/batches/dto/create-batch.dto.ts
@@ -1,7 +1,21 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateBatchDTO {
   @IsNotEmpty({ message: 'Nome do projeto de assentamento é obrigatório.' })
   @IsString()
   readonly settlement_project: string;
+
+  @IsNotEmpty({ message: 'Número de documentos físicos é obrigatório.' })
+  @IsNumber()
+  readonly physical_files_count: number;
+
+  @IsOptional()
+  @IsBoolean()
+  readonly priority?: boolean;
 }
