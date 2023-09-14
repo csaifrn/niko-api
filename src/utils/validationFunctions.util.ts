477c8c82033f585cae09967707e0745d195685c8
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-13 23:04:41 -0300

    feat: isPhysicalFilesCountValid util function

--- src/utils/validationFunctions.util.ts
+++ src/utils/validationFunctions.util.ts
@@ -21,6 +21,12 @@ export const isSettlementProjectValid = (
   return settlementProject.length < MIN_CHARACTERS;
 };
 
+export const isPhysicalFilesCountValid = (
+  physical_files_count: number,
+): boolean => {
+  return physical_files_count <= 0;
+};
+
 export const isBatchObservationValid = (observation: string): boolean => {
   const MIN_CHARACTERS = 3;
   return observation.length < MIN_CHARACTERS;
