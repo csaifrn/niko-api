fdf9987b35e4c1caec4f2aaf82615ca1568a9698
NicholasTavares <nicholasbalby@hotmail.com>
2023-09-17 17:53:15 -0300

    feat: settlement_project_categories entity

---
+++ src/modules/settlement_project_categories/entities/settlement_project_categories.entity.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { User } from '../../users/entities/user.entity';
+
+@Entity('settlement_project_categories')
+export class SettlementProjectCategory {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @Column({ name: 'user_id' })
+  user_id: string;
+
+  @ManyToOne(() => User, (user) => user)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}
