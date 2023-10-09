040d64293f419bcee24047d8b38dfcf6d4b442cb
Nicholas Tavares <48931095+NicholasTavares@users.noreply.github.com>
2023-10-09 14:15:54 -0300

    Update roles.controller.ts

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -12,8 +12,6 @@ export class RolesController {
     summary: 'Criar função',
     description: 'Criar função para usuário (nível de acesso).',
   })
-  @ApiBearerAuth()
-  @UseGuards(AuthGuard('jwt'))
   @Post()
   create(@Body() createRoleDTO: CreateRoleDTO, @Request() req: any) {
     return this.rolesService.create(createRoleDTO, req.user.id);
