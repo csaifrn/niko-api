b494dbe6c685275c1da873923780926bc5d3e642
NicholasTavares <nicholasbalby@hotmail.com>
2023-11-04 17:02:09 -0300

    feat: add user and history entities

--- src/modules/batches/batches.module.ts
+++ src/modules/batches/batches.module.ts
@@ -5,13 +5,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Batch } from './entities/batch.entity';
 import { BatchObservation } from './entities/batch_observations.entity';
 import { SettlementProjectCategory } from '../settlement_project_categories/entities/settlement_project_categories.entity';
+import { BatchHistory } from './entities/batch_history.entity';
+import { User } from '../users/entities/user.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
+      User,
       Batch,
       BatchObservation,
       SettlementProjectCategory,
+      BatchHistory,
     ]),
   ],
   controllers: [BatchesController],
