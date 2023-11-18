e8f4176a64ff662167a7d80b4a459b278b09c340
NicholasTavares <nicholasbalby@hotmail.com>
2023-11-18 14:25:48 -0300

    feat: tag entity

---
+++ src/modules/tags/entitites/tag.entity.ts
@@ -0,0 +1,42 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Batch } from '../../batches/entities/batch.entity';
+@Entity('tags')
+export class Tag {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  name: string;
+
+  @ManyToMany(() => Batch)
+  @JoinTable({
+    name: 'batches_tags',
+    joinColumn: {
+      name: 'batch_id',
+      referencedColumnName: 'id',
+    },
+    inverseJoinColumn: {
+      name: 'tag_id',
+      referencedColumnName: 'id',
+    },
+  })
+  batches?: Batch[];
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updated_at: Date;
+
+  @DeleteDateColumn({ type: 'timestamp' })
+  deleted_at: Date;
+}
