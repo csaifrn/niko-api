8981d348aa0ca60e65ee85d6431fdc9f072b0897
NicholasTavares <nicholasbalby@hotmail.com>
2023-11-18 20:57:22 -0300

    refactor: att documentation

--- src/modules/tags/dto/create-tag.dto.ts
+++ src/modules/tags/dto/create-tag.dto.ts
@@ -1,6 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
+import { MAX_TAGNAME_CHARACTERS } from '../../../utils/validationConstants';
 
 export class CreateTagDTO {
+  @ApiProperty({
+    type: String,
+    required: true,
+    title: 'Nome da tag.',
+    description: `O nome da tag pode ter no máximo ${MAX_TAGNAME_CHARACTERS} caracteres.`,
+  })
   @IsNotEmpty({ message: 'Nome da tag é obrigatório.' })
   @IsString()
   readonly name: string;
