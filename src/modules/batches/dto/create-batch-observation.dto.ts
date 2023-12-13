59552aae14a4e7be6944361df70c03a65bc79411
NicholasTavares <nicholasbalby@hotmail.com>
2023-12-13 08:28:20 -0300

    feat: set is_pending on create observation

--- src/modules/batches/dto/create-batch-observation.dto.ts
+++ src/modules/batches/dto/create-batch-observation.dto.ts
@@ -1,7 +1,11 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateBatchObservationDTO {
   @IsNotEmpty({ message: 'Observação do lote é obrigatória.' })
   @IsString()
   readonly observation: string;
+
+  @IsOptional()
+  @IsBoolean()
+  readonly is_pending?: boolean;
 }
