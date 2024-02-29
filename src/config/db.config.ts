95e391d1481d8c8783eca9bda012cb891dd87205
Nicholas.Moreno <nicholas.balby@getinfo.net.br>
2024-02-29 09:20:23 -0300

    feat: mock database address

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,20 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
-export default registerAs('database', () => {
-  return {
-    type: 'postgres',
-    logging: true,
-    host: process.env.DB_MAIN_HOST,
-    port: +process.env.DB_MAIN_PORT,
-    username: process.env.DB_MAIN_USER,
-    password: process.env.DB_MAIN_PASSWORD,
-    database: process.env.DB_MAIN_DATABASE,
-    autoLoadEntities: true,
-    synchronize: true,
-    entities: ['dist/src/modules/**/entities/*.entity.js'],
-    migrations: ['dist/src/migrations/*.js'],
-    cli: {
+export default registerAs('database', () => ({
+  type: 'postgres',
+  logging: true,
+  url: `postgres://niko_wu43_user:1oue8nbg021AATIuZiAJasWgVB31EXts@dpg-cng71lda73kc73ded5ng-a.oregon-postgres.render.com/niko_wu43`,
+  autoLoadEntities: true,
+  synchronize: true,
+  entities: ['dist/src/modules/**/entities/*.entity.js'],
+  migrations: ['dist/src/migrations/*.js'],
+  cli: {
       migrationsDir: 'src/migrations',
-    },
-  };
-});
+  },
+}));
